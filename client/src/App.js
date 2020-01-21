6ecae2a5a012b324d3f2032f7b6371aabe43d35d
ziphrax <daverival@hotmail.com>
2020-01-21 23:29:42 +0000

    Have voting action bound

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,44 @@ const labels = ["0", String.fromCharCode(189), "1", "2", "3", "5",
 
 const data = [1,2,3];
 
+// Example Room response data
+// {
+//   "roomId": "KIRT",
+//   "host": "dave",
+//   "votes": {
+//       "me :-D": {
+//           "name": "me :-D",
+//           "vote": "3",
+//           "timestamp": "0000-00-00 00:00:00.000"
+//       }
+//   },
+//   "history": [
+//       {
+//           "name": "me :-D",
+//           "vote": "3",
+//           "timestamp": "0000-00-00 00:00:00.000"
+//       }
+//   ],
+//   "success": true
+// }
+
+const roomToData = (labels, room) => {
+  let data = labels.map(value => {
+    let count = 0;
+
+    Object.keys(room.votes).map(key => { 
+      if(room.votes[key].vote === value){
+        count++;
+      }
+    });
+
+    return count;
+  });
+
+  return data;
+}
+
+
 function App() {
   const [name, setName] = useState("");
   const [roomId, setRoomId] = useState("");
@@ -59,20 +97,31 @@ function App() {
       
       <UsernameInput name={name} setName={setName} />
 
-      <RoomViewer />
+      { name && <RoomViewer />}
 
-      { name && <SessionInput session={roomId} setSession={setRoomId} joinRoom={joinRoom} hostRoom={hostRoom} leaveRoom={leaveRoom} />}
+      { name && 
+        <SessionInput session={roomId} 
+                      setSession={setRoomId} 
+                      joinRoom={joinRoom} 
+                      hostRoom={hostRoom} 
+                      leaveRoom={leaveRoom} />}
 
-      <pre>{JSON.stringify(currentRoom)}</pre>
-
-      <StandardCardSelector cards={labels} cardValue={cardValue} setCardValue={setCardValue} vote={vote} />
+      { currentRoom &&  
+        <StandardCardSelector cards={labels} 
+                              cardValue={cardValue} 
+                              setCardValue={setCardValue} 
+                              vote={vote} /> }
       
-      <ResultsHistogram title="Results" 
-                        labels={labels} 
-                        data={data} 
-                        color="#70CAD1" />
+      { currentRoom && 
+        <ResultsHistogram title="Results" 
+                          labels={labels} 
+                          data={roomToData(labels, currentRoom)} 
+                          color="#70CAD1" />}
 
       <h2>&hearts; &diams; &spades; &clubs;&hearts; &diams; &spades; &clubs;&hearts; &diams; &spades; &clubs;&hearts; &diams; &spades; &clubs;</h2>
+    
+      {/* <pre>{JSON.stringify(currentRoom)}</pre> */}
+
     </div>
   );
 }
