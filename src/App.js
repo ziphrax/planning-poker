6e67d6b9fca31d88032521aa348ea7397cd4b042
ziphrax <daverival@hotmail.com>
2020-01-21 19:26:04 +0000

    Switched to Chart.Js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import Title from './components/title'
 import UsernameInput from './components/username-input'
 import StandardCardSelector from './components/standard-card-selector'
 import SessionInput from './components/session-input'
-// import ResultsHistogram from './components/results-histogram'
+import ResultsHistogram from './components/results-histogram'
+
+const labels = ["0 pts","1 pt","2 pts","3 pts", "5 pts",
+                "8 pts", "14 pts", "20 pts", "40 pts"," 100 pts",":coffee","?","&infin;"
+];
+
+const data = [1,2,3];
 
 function App() {
   const [name, setName] = useState("");
@@ -16,12 +22,16 @@ function App() {
     <div className="App">
       <Title />
       
-      {/* <UsernameInput name={name} setName={setName} />
-      <SessionInput session={session} setSession={setSession} /> */}
+      <UsernameInput name={name} setName={setName} />
+      <SessionInput session={session} setSession={setSession} />
 
       <StandardCardSelector cardValue={cardValue} setCardValue={setCardValue} />
       
-      {/* <ResultsHistogram /> */}
+      <ResultsHistogram title="Results" 
+                        labels={labels} 
+                        data={data} 
+                        color="#70CAD1" />
+
       <h2>&hearts; &diams; &spades; &clubs;&hearts; &diams; &spades; &clubs;&hearts; &diams; &spades; &clubs;&hearts; &diams; &spades; &clubs;</h2>
     </div>
   );
--- src/components/results-histogram.js
+++ src/components/results-histogram.js
@@ -1,32 +1,51 @@
-import React from 'react'
+import React, { createRef, useEffect } from 'react'
+import Chart from 'chart.js'
+
 import '../styles/results-histogram.css'
 
-import Plot from 'react-plotly.js'
 
-const xaxis = ["0 pts","1 pt","2 pts","3 pts", "5 pts",
-                "8 pts", "14 pts", "20 pts", "40 pts"," 100 pts",":coffee","?"
-];
-
-const data=
-    [{
-        x: xaxis,
-        y: [0,0,4,2,3,0,0,0,0,0,0,0],
-        type: 'bar'
-    }]
+// const data=
+//     [{
+//         x: xaxis,
+//         y: [0,0,4,2,3,0,0,0,0,0,0,0],
+//         type: 'bar'
+//     }]
 
 
-const layout={
-    width:"100%",
-    height:"250px",
-    title:"Results"
-}
+// const layout={
+//     width:"100%",
+//     height:"250px",
+//     title:"Results"
+// }
 
-export default function(){
+export default function({labels, title, data, color}){
+    const chartRef = createRef();
+
+    useEffect(() => {
+        new Chart(chartRef.current,{
+            type: 'bar',
+            data: { 
+                labels: labels,
+                datasets:[{
+                    label: title,
+                    data : data,
+                    backgroundColor: color
+                }]
+            },
+            options: {
+                responsive: true,
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Chart.js Bar Chart'
+                }
+            }
+        })
+    });
+
     return (
-        <div className="results-pictograph">
-            <Plot 
-                data={data}
-                layout={layout} />
-        </div>
+        <canvas className="results-pictograph" ref={chartRef}></canvas>
     )
 }
